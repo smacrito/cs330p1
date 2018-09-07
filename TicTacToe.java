056b65addf8e71b9ce752b3c0bf75fe9d3d2eb8f
fiyyaa <smacrito@luc.edu>
2018-09-07 09:42:57 -0500

    Added static to all variables

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,66 +1,65 @@
 import java.util.*;
 import java.io.*;
-
 public class TicTacToe{
-
-    boolean checkWinner = false;
-
-    HashSet checkDupes = new HashSet();
-
+    static boolean checkWinner = false;
+    static HashSet checkDupes = new HashSet();
+    static char one = '1';
+    static char two = '2';
+    static char three= '3';
+    static char four = '4';
+    static char five = '5';
+    static char six = '6';
+    static char seven = '7';
+    static char eight= '8';
+    static char nine = '9';
     public static void main(String argsp[]){
         draw();
         
         }
-
-        public void checkWin(){
-            if(one.equals(two) && two.equals(three));
+        public static void checkWin(){
+            if(one==(two) && two==(three));
         }
-
-        public static String draw(){
-            return System.out.println(one + "|" + two + "|" + three + "\n" + four + "|" + five +"|" + six + "\n" + seven + "|" + eight + "|" + nine +"\n");
+        public static void draw(){
+            System.out.println(one + "|" + two + "|" + three + "\n" + four + "|" + five +"|" + six + "\n" + seven + "|" + eight + "|" + nine +"\n");
         }
-
         public static void playerX(){
             int turnCount = 0;
-            int choice = 0;
+            char choice = '0';
             Scanner sc = new Scanner(System.in);
 
             while(turnCount < 9 && checkWinner == false){
 
                 System.out.println("Player X, enter the number that corresponds to the cell: ");
-                choice = sc.nextChar();
+                choice = sc.next().charAt(0);
                 checkDupes.add(choice);
 
-                if(one.equals(choice) && !checkDupes.contains(choice)){
+                if(one==(choice) && !checkDupes.contains(choice)){
                     one ='x';
                 }
-                else if(two.equals(choice) && !checkDupes.contains(choice)){
+                else if(two==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(three.equals(choice) && !checkDupes.contains(choice)){
+                else if(three==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(four.equals(choice) && !checkDupes.contains(choice)){
+                else if(four==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(five.equals(choice) && !checkDupes.contains(choice)){
+                else if(five==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(six.equals(choice) && !checkDupes.contains(choice)){
+                else if(six==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(seven.equals(choice) && !checkDupes.contains(choice)){
+                else if(seven==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(eight.equals(choice) && !checkDupes.contains(choice)){
+                else if(eight==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
-                else if(nine.equals(choice) && !checkDupes.contains(choice)){
+                else if(nine==(choice) && !checkDupes.contains(choice)){
                     two ='x';
                 }
         }
-
-
-        
     }
 }
